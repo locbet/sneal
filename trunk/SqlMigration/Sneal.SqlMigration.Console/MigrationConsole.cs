2d333388cc3c13d8ca8616e2436470cdbc7a7847
neal.shawn <neal.shawn@be8551ab-8b43-0410-8ef5-5fc30df68f84>
2008-04-09 06:39:54 +0000

    Updated CmdLineParser.dll and got console app functional.

--- trunk/SqlMigration/Sneal.SqlMigration.Console/MigrationConsole.cs
+++ trunk/SqlMigration/Sneal.SqlMigration.Console/MigrationConsole.cs
@@ -1,14 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Sneal.SqlMigration.Impl;
 
 namespace Sneal.SqlMigration.Console
 {
     public class MigrationConsole
     {
-        public int Run(Options options)
+        private readonly ScriptMessageManager messageMgr = new ScriptMessageManager();
+
+        public MigrationConsole()
         {
+            messageMgr.ScriptMessage += MessageMgr_ScriptMessage;
+        }
+
+        public int Run(CmdLineScriptingOptions scriptingOptions, IConnectionSettings srcConnSettings,
+                       IConnectionSettings targetConnSettings)
+        {
+            MigrationEngine engine = new MigrationEngine(new SqlServerScriptBuilder());
+            engine.MessageManager = messageMgr;
+
+            if (string.IsNullOrEmpty(targetConnSettings.Database))
+            {
+                engine.Script(srcConnSettings, scriptingOptions);
+            }
+            else
+            {
+                engine.ScriptDifferences(srcConnSettings, targetConnSettings, scriptingOptions);
+            }
+
             return 0;
+        }
+
+        private void MessageMgr_ScriptMessage(object sender, ScriptMessageEventArgs e)
+        {
+            System.Console.WriteLine(e.Message);
         }
     }
 }

a0810a9146fa6de697cd725a6c56d8763d97e17c
neal.shawn <neal.shawn@be8551ab-8b43-0410-8ef5-5fc30df68f84>
2009-06-23 15:02:09 +0000

    Added CmdLineParser example project Extracted usage formatter into strategy class and interface Made the default usage printer output a lot prettier

---
+++ trunk/CmdLineParser/Sneal.CmdLineParser.Sample/Program.cs
@@ -0,0 +1,84 @@
+﻿#region license
+// Copyright 2009 Shawn Neal (neal.shawn@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+namespace Sneal.CmdLineParser.Sample
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var parser = new CommandLineParser(args);
+            MyOptions options = parser.BuildOptions(new MyOptions());
+
+            if (options.ShowHelp)
+            {
+                DisplayHelpScreen(parser, options);
+                return 0;
+            }
+
+            if (parser.IsMissingRequiredOptions(options))
+            {
+                foreach (Option missingOption in parser.FindMissingRequiredOptions(options))
+                {
+                    Console.WriteLine("Missing required option: " + missingOption.Name);
+                }
+                DisplayHelpScreen(parser, options);
+                return 1;
+            }
+            Console.WriteLine(options.ToString());
+            return 0;
+        }
+
+        static void DisplayHelpScreen(CommandLineParser parser, MyOptions options)
+        {
+            Console.WriteLine("Utility for connecting to an HTTP server.");
+            Console.WriteLine();
+            Console.WriteLine(parser.GetUsageLines(options));
+        }
+    }
+
+    public class MyOptions
+    {
+        [Option(ShortName = "s", LongName = "serverisreallyfunandcool",
+            Description = "The remote endpoint address of the HTTP server as specified using a fully qualified domain name.",
+            Required = true)]
+        public string Server { get; set; }
+
+        [Option(ShortName = "p", LongName = "port",
+            Description = "The HTTP server port, which if not specified defaults to port 80.")]
+        public int Port { get; set; }
+
+        [Option(ShortName = "l", LongName = "login", Description = "Set to true to login to the server.")]
+        public bool Login { get; set; }
+
+        [Option(ShortName = "h", LongName = "help", Description = "Shows this screen.")]
+        public bool ShowHelp { get; set; }
+
+        public MyOptions()
+        {
+            Port = 80;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "Server={0}; Port={1}; Login={2}",
+                Server, Port, Login);
+        }
+    }
+}
---
+++ trunk/CmdLineParser/Sneal.CmdLineParser.Tests/DefaultUsageFormatterTests.cs
@@ -0,0 +1,123 @@
+﻿#region license
+// Copyright 2009 Shawn Neal (neal.shawn@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Sneal.CmdLineParser.Tests
+{
+    [TestFixture]
+    public class DefaultUsageFormatterTests
+    {
+        private DefaultUsageFormatter _formatter = new DefaultUsageFormatter();
+
+        [Test]
+        public void Tab_length_should_default_to_20()
+        {
+            Assert.AreEqual(20, _formatter.DefaultMinTabSize);
+        }
+
+        [Test]
+        public void Tab_length_should_adjust_to_accomodate_option_names_longer_than_20()
+        {
+            var options = new List<Option>
+            {
+                new Option { LongName = "ThisIsAReallyLongOptionOf32Chars", Description = "Some sort of string option" },
+                new Option { ShortName = "a", Description = "A really short option" },
+            };
+
+            const string expected =
+@"  /ThisIsAReallyLongOptionOf32Chars Some sort of string option
+  /a                                A really short option
+";
+            string usage = _formatter.GetUsage(options);
+            Assert.AreEqual(expected, usage);            
+        }
+
+        [Test]
+        public void ShouldGetUsageLines()
+        {
+            var options = new List<Option>
+            {
+                new Option { LongName = "StringOption", Description = "Some sort of string option" },
+                new Option { LongName = "BoolOption", Description = "Some sort of bool option" },
+                new Option { LongName = "IntOption", Description = "Some sort of int option" },
+                new Option { LongName = "StringList", Description = "Some sort of string collection option" }
+            };
+
+            const string expected =
+@"  /StringOption     Some sort of string option
+  /BoolOption       Some sort of bool option
+  /IntOption        Some sort of int option
+  /StringList       Some sort of string collection option
+";
+            string usage = _formatter.GetUsage(options);
+            Assert.AreEqual(expected, usage);
+        }
+
+        [Test]
+        public void Lines_longer_than_80chars_should_wrap_then_tab()
+        {
+            var options = new List<Option>
+            {
+                new Option { LongName = "aa", Description = "A really long description that wishes it could fit inside eighty chars, but it just can't because its so damn long." },
+                new Option { ShortName = "b", Description = "A really short option" },
+            };
+
+            const string expected =
+@"  /aa               A really long description that wishes it could fit inside
+                    eighty chars, but it just can't because its so damn long.
+  /b                A really short option
+";
+            string usage = _formatter.GetUsage(options);
+            Assert.AreEqual(expected, usage);
+        }
+
+        [Test]
+        public void Lines_longer_than_80chars_should_wrap_then_tab_even_with_extra_long_names()
+        {
+            var options = new List<Option>
+            {
+                new Option { LongName = "ThisIsAReallyLongOptionOf32Chars", Description = "A really long description that wishes it could fit inside eighty chars, but it just can't because its so damn long." },
+                new Option { ShortName = "b", Description = "A really short option" },
+            };
+
+            const string expected =
+@"  /ThisIsAReallyLongOptionOf32Chars A really long description that wishes it
+                                    could fit inside eighty chars, but it just
+                                    can't because its so damn long.
+  /b                                A really short option
+";
+            string usage = _formatter.GetUsage(options);
+            Assert.AreEqual(expected, usage);
+        }
+
+        [Test]
+        public void Should_show_short_form_in_description_when_both_long_and_short_names_are_specified()
+        {
+            var options = new List<Option>
+            {
+                new Option { LongName="bob", ShortName = "b", Description = "A really short option" },
+            };
+
+            const string expected =
+@"  /bob              A really short option (Short form: /b)
+";
+            string usage = _formatter.GetUsage(options);
+            Assert.AreEqual(expected, usage);
+        }
+    }
+}

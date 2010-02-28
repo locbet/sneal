89eea6acd676c7eb3fdb1956a8a67cfc4217e294
neal.shawn <neal.shawn@be8551ab-8b43-0410-8ef5-5fc30df68f84>
2010-02-28 18:24:32 +0000

    More missing files

---
+++ Stormwind/Stormwind.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stormwind.Core.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stormwind.Core.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Shawn Neal 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4a8b103b-33f5-4949-afb6-2f9cb9048654")]

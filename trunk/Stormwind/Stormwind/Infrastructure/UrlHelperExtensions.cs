c2cddec8c4aa8046528b1e435c9dbd3b4d472f1a
neal.shawn <neal.shawn@be8551ab-8b43-0410-8ef5-5fc30df68f84>
2010-02-28 18:20:15 +0000

    Added missing url helper extensions removed old infrastructure code

---
+++ trunk/Stormwind/Stormwind/Infrastructure/UrlHelperExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using Sneal.Core;
+
+namespace Stormwind.Infrastructure
+{
+    /// <summary>
+    /// MVC Url extension methods.
+    /// </summary>
+    public static class UrlHelperExtensions
+    {
+        /// <summary>
+        /// Gets a full URL to the specified image file name.
+        /// </summary>
+        public static string Image(this UrlHelper helper, string fileName)
+        {
+            return GetContentPath(helper, @"Images", fileName);
+        }
+
+        /// <summary>
+        /// Gets a full URL to the specified stylesheet file name.
+        /// </summary>
+        public static string Stylesheet(this UrlHelper helper, string fileName)
+        {
+            return GetContentPath(helper, @"Stylesheets", fileName);
+        }
+
+        /// <summary>
+        /// Gets a full URL to the specified script file name.
+        /// </summary>
+        public static string Script(this UrlHelper helper, string fileName)
+        {
+            return GetContentPath(helper, @"Scripts", fileName);
+        }
+
+        private static string GetContentPath(this UrlHelper helper, string subDir, string fileName)
+        {
+            return helper.Content(@"{Content}/{SubDirectory}/{FileName}".FormatWith(
+                                      new { Content = RootContentPath, SubDirectory = subDir, FileName = fileName }));
+        }
+
+        private static string RootContentPath
+        {
+            get { return "~/Content"; }
+        }
+    }
+}
